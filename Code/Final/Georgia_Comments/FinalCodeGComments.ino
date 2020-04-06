840b52156495868f63bd52142b318325660e460f
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2020-04-06 17:46:35 -0400

    G: Added title, description, and comments

--- Code/Final/Georgia_Comments/FinalCodeGComments.ino
+++ Code/Final/Georgia_Comments/FinalCodeGComments.ino
@@ -1,56 +1,55 @@
 //Georgia and Basir
 // Final Motor Control Code (Georgia's Comments)
 /* This was the first code we tried after ordering a new dimmer and it worked. With a few changes, it became our final 
-code. The one issue we encountered with this code was that it is impossible to run delays in it because the code
-already uses the Arduinos timer functions. The comments on this version are mine, to see comments from the website go
-to the original commenst file. */
+code. The one hiccup we encountered with this code was that it is impossible to run delays in it because the code
+already uses the Arduino's timer functions. The comments on this version are mine, to see comments from the website go
+to the "original comment" file. */
 
-#include <RBDdimmer.h>
+#include <RBDdimmer.h> //Dimmer libraries and other setup info
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define potPin 0 //potentiometer read pin 
-#define DETECT 2 // zero cross detect
-#define GATE 11 // TRIAC gate
-#define PULSE 4 // trigger pulse width (counts)
-int i = 483;
-int potValue = 0;
+#define potPin 0 //potentiometer pin
+#define DETECT 2 // Zero cross detect
+#define GATE 11 // Sets TRIAC gate
+#define PULSE 4 // Sets puls width
+int i = 483; // This variable is left in but is unnecessary when potentiometer is used (see commented out code below)
+int potValue = 0; //Inital potentiometer value
 
 
 void setup()
 {
   // set up pins
-  pinMode(DETECT, INPUT); // zero cross detect
-  digitalWrite(DETECT, HIGH); // enable pull-up resistor
-  pinMode(GATE, OUTPUT); // TRIAC gate control
-  // set up Timer1
-  // (see ATMEGA 328 data sheet pg 134 for more details)
-  OCR1A = 100; // initialize the comparator
-  TIMSK1 = 0x03; // enable comparator A and overflowinterrupts
-  TCCR1A = 0x00; // timer control registers set for
-  TCCR1B = 0x00; // normal operation, timer disabled
-  // set up zero crossing interrupt
+  pinMode(DETECT, INPUT); // Zero cross detect is input
+  digitalWrite(DETECT, HIGH); // Enables the pull-up resistor
+  pinMode(GATE, OUTPUT); // Triac gate is output
+  //Below is setup for timer 1
+  OCR1A = 100; 
+  TIMSK1 = 0x03; 
+  TCCR1A = 0x00; 
+  TCCR1B = 0x00; 
+
   attachInterrupt(0, zeroCrossingInterrupt, RISING);
-  // IRQ0 is pin 2. Call zeroCrossingInterrupt
-  // on rising signal
+  // Sets an attach interrupt function to check zero cross pin for rising activity
+
   
 }
 
-// Interrupt Service Routines
+
 void zeroCrossingInterrupt()
 {
-  // zero cross detect
-  TCCR1B = 0x04; // start timer with divide by 256 input
-  TCNT1 = 0; // reset timer - count from zero
+  //Interrupt function
+  TCCR1B = 0x04; // Starts timer 
+  TCNT1 = 0; // Resets timer from zero
 }
 
 extern "C"
 {
   ISR(TIMER1_COMPA_vect)
   {
-    // comparator match
-    digitalWrite(GATE, HIGH); // set TRIAC gate to high
-    TCNT1 = 65536 - PULSE; // trigger pulse width
+   
+    digitalWrite(GATE, HIGH); // Triac gate to high 
+    TCNT1 = 65536 - PULSE; 
 
   }
 }
@@ -59,16 +58,16 @@ extern "C"
 {
   ISR(TIMER1_OVF_vect)
   {
-    // timer1 overflow
-    digitalWrite(GATE, LOW); // turn off TRIAC gate
-    TCCR1B = 0x00; // disable timer stopd unintended triggers
+  
+    digitalWrite(GATE, LOW); // Triac gate to low (off)
+    TCCR1B = 0x00; // Timer disabled
           
   }
 }
 
 void loop(){
 
-  potValue = analogRead(potPin);
+  potValue = analogRead(potPin); //Defines potentiometer value
    /*
   i--;
   // i = i -1;
@@ -78,8 +77,10 @@ void loop(){
   {
     i = 483;
   }
-*/
-  OCR1A = map(potValue, 0, 1023, 0, 483); // set the compare register brightness desired.
+*/ /*This code is commented out as it is unnecessary when the potentiometer is incorporated. It also did not work when
+  we ran it like this without a potentiometer. */
+  
+  OCR1A = map(potValue, 0, 1023, 0, 483); // Sets OCR1A compare function to a map that converts potValue to brightness
   
   }
 

03d7b72cbb1963b9a908152789227048cc846b74
gwyatt40 <55802319+gwyatt40@users.noreply.github.com>
2020-04-06 17:01:52 -0400

    G: Added title and description

--- Code/Archive/PotentiometerACLoadCode.ino
+++ Code/Archive/PotentiometerACLoadCode.ino
@@ -1,3 +1,8 @@
+//Georgia and Basir
+//Prototype Code 1 (Potentiometer AC Load Code)
+/* This was the very first prototype code we tried. It did not work. The main issue with this code is that it does not
+use any functions necessary to the dimmer, such as zero cross detection. It was derived from editing an old code that 
+used a potentiometer to change the brightness of an LED. I left the old comments in to illustrate this. */
 
 int potPin= 2;  //Sets potentiometer to analog pin A0
 int AC_LOAD= 12;  // Sets LED to normal arduino pin 9
